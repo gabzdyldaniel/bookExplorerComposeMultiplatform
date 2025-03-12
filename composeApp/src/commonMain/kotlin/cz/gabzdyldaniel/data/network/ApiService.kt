b22a8695e5066ae54e18cc65b2ee60f37b6a6e5b
Daniel Gabzdyl <gabzdyldaniel@gmail.com>
2025-03-12 09:12:20 +0100

    chore: Improve ApiService

--- composeApp/src/commonMain/kotlin/cz/gabzdyldaniel/data/network/ApiService.kt
+++ composeApp/src/commonMain/kotlin/cz/gabzdyldaniel/data/network/ApiService.kt
@@ -5,12 +5,14 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import io.ktor.client.plugins.logging.*
 
 class ApiService {
 
-    private val baseUrl = "www.googleapis.com/books/v1/volumes?q="
+    private val baseUrl = "www.googleapis.com/books/v1/volumes"
 
     private val httpClient = HttpClient {
         install(ContentNegotiation) {
@@ -21,9 +23,26 @@ class ApiService {
                 coerceInputValues = true
             })
         }
+
+        install(Logging) {
+            level = LogLevel.ALL
+        }
     }
 
     suspend fun getBooks(author: String): VolumesResponse {
-        return httpClient.get("$baseUrl&inauthor:\"${author.replace(" ", "+")}\"langRestrict=cs").body()
+        try {
+            val response = httpClient.get("https://$baseUrl") {
+                parameter("q", "inauthor:$author")
+                parameter("langRestrict", "cs")
+            }
+
+            println("HTTP status: ${response.status}")
+
+            return response.body<VolumesResponse>()
+        } catch (e: Exception) {
+            println(e.message)
+            e.printStackTrace()
+            throw e
+        }
     }
 }
